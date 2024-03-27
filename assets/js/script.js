d51c6ab9944ed7b29fd8463eca2132a54ed583f5
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2024-03-27 13:15:59 +0100

    add API function to recover the weather from lat and long

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,20 @@ const weatherCityElement = document.querySelector('.weather_city');
 const weatherImagesElement = document.querySelector('.weather_img');
 const weatherDegreesElement = document.querySelector('.weather_degrees');
 const adviceTextElement = document.querySelector('.advise_text');
+
+// API URL
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
+
+// call to recover the weather forecasts
+async function translate() {
+
+    // API URL
+    //to edit the url
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
+
+    const response = await fetch(API_URL);
+    const jsonData = await response.json();
+
+    console.log(jsonData);
+
+};
