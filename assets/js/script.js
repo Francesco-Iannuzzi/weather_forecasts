54fac30f6817f3aeca5f23ef0251a6a4f28f75ba
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2024-03-27 13:14:25 +0100

    select DOM element js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// select element from DOM
+const bodyElement = document.body;
+const weatherCityElement = document.querySelector('.weather_city');
+const weatherImagesElement = document.querySelector('.weather_img');
+const weatherDegreesElement = document.querySelector('.weather_degrees');
+const adviceTextElement = document.querySelector('.advise_text');
